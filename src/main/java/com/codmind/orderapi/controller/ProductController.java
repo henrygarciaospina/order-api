eae8ffa4e6361c0234a9dd1be908ffb83bac9301
henrygosling <henrygarciaospina@hotmail.com>
2022-07-12 14:09:23 -0500

    Método Post para Product

--- src/main/java/com/codmind/orderapi/controller/ProductController.java
+++ src/main/java/com/codmind/orderapi/controller/ProductController.java
@@ -1,9 +1,7 @@
 package com.codmind.orderapi.controller;
 
 import com.codmind.orderapi.entity.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,5 +35,11 @@ public class ProductController {
             }
         }
         return null;
+    }
+
+    @PostMapping(value="/products")
+    public Product create(@RequestBody Product product){
+        this.products.add(product);
+        return product;
     }
 }
