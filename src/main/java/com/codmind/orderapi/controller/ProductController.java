48b929f371ada50e0955724ad2bf7c3863ec578c
henrygosling <henrygarciaospina@hotmail.com>
2022-07-15 21:55:00 -0500

    Implementación ProductService

--- src/main/java/com/codmind/orderapi/controller/ProductController.java
+++ src/main/java/com/codmind/orderapi/controller/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.codmind.orderapi.repository.ProductRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
---
+++ src/main/java/com/codmind/orderapi/services/ProductService.java
@@ -0,0 +1,48 @@
+package com.codmind.orderapi.services;
+
+import com.codmind.orderapi.entity.Product;
+import com.codmind.orderapi.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    static final String MESSAGE = "No existe el producto";
+
+    public Product findById(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException(MESSAGE));
+    }
+
+    @Transactional
+    public void delete(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException(MESSAGE));
+        productRepository.delete(product);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product update(Product product) {
+        Product existProduct = productRepository.findById(product.getId())
+                .orElseThrow(() -> new RuntimeException(MESSAGE));
+
+        existProduct.setName(product.getName());
+        existProduct.setPrice(product.getPrice());
+
+        return productRepository.save(existProduct);
+    }
+
+}
