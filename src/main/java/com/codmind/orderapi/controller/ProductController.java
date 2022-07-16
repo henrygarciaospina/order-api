61d4d1f28d07136f976fe990bb8557c01e4bb15f
henrygosling <henrygarciaospina@hotmail.com>
2022-07-15 21:14:55 -0500

    Método Update  Products

--- src/main/java/com/codmind/orderapi/controller/ProductController.java
+++ src/main/java/com/codmind/orderapi/controller/ProductController.java
@@ -19,23 +19,25 @@ public class ProductController {
 
     public ProductController() {
 
-        for(int c = 0; c<= 10; c++ ) {
-           products.add(Product.builder()
-                   .id((c+1L))
-                   .name("Product " + (c+1L))
-                   .build()
-           ) ;
+        for (int c = 0; c <= 10; c++) {
+            products.add(Product.builder()
+                    .id((c + 1L))
+                    .name("Product " + (c + 1L))
+                    .build()
+            );
         }
     }
-    @GetMapping(value="/products/{productId}")
-    public ResponseEntity<Product> findById(@PathVariable("productId") Long productId){
+
+    @GetMapping(value = "/products/{productId}")
+    public ResponseEntity<Product> findById(@PathVariable("productId") Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("No existe el producto"));
 
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
-    @DeleteMapping(value="/products/{productId}")
-    public ResponseEntity<Void> delete(@PathVariable("productId") Long productId){
+
+    @DeleteMapping(value = "/products/{productId}")
+    public ResponseEntity<Void> delete(@PathVariable("productId") Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("No existe el producto"));
 
@@ -43,26 +45,32 @@ public class ProductController {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    @GetMapping(value="/products")
-    public ResponseEntity<List<Product>> findAll(){
+
+    @GetMapping(value = "/products")
+    public ResponseEntity<List<Product>> findAll() {
 
         List<Product> products = productRepository.findAll();
 
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
-    @PostMapping(value="/products")
-    public ResponseEntity<Product> create(@RequestBody Product product){
+
+    @PostMapping(value = "/products")
+    public ResponseEntity<Product> create(@RequestBody Product product) {
         Product newProduct = productRepository.save(product);
         return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
     }
-    @PutMapping(value="/products")
-    public Product update(@RequestBody Product product){
-        for(Product prod: this.products) {
-            if(prod.getId().longValue() == product.getId().longValue()){
-                prod.setName(product.getName());
-                return prod;
-            }
-         }
-        throw new RuntimeException("No existe el producto");
+
+    @PutMapping(value = "/products")
+    public ResponseEntity<Product> update(@RequestBody Product product) {
+        Product existProduct = productRepository.findById(product.getId())
+                .orElseThrow(() -> new RuntimeException("No existe el producto"));
+
+        existProduct.setName(product.getName());
+        existProduct.setPrice(product.getPrice());
+
+        productRepository.save(existProduct);
+
+        return new ResponseEntity<>(existProduct, HttpStatus.OK);
     }
+
 }
