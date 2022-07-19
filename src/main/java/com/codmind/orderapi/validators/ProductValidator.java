74b9307ab9c8df3fd4dee391d5f7ddd56d217910
henrygosling <henrygarciaospina@hotmail.com>
2022-07-19 10:43:14 -0500

    Se modifica ProductValidator con el manejo de la excepción ValidateServiceException

--- src/main/java/com/codmind/orderapi/validators/ProductValidator.java
+++ src/main/java/com/codmind/orderapi/validators/ProductValidator.java
@@ -1,6 +1,7 @@
 package com.codmind.orderapi.validators;
 
 import com.codmind.orderapi.entity.Product;
+import com.codmind.orderapi.exceptions.ValidateServiceException;
 
 public class ProductValidator {
 
@@ -8,9 +9,9 @@ public class ProductValidator {
     }
 
     public static void save(Product product) {
-        if((product.getName() == null) || product.getName().trim().isEmpty()) throw (new RuntimeException("El nombre es requerido"));
-        if(product.getName().length() > 100) throw (new RuntimeException("El nombre debe tener máximo 100 caracteres"));
-        if(product.getPrice() == null) throw (new RuntimeException("El precio es requerido"));
-        if(product.getPrice() <= 0) throw (new RuntimeException("El precio debe tener un valor mayor o igual a cero (0)"));
+        if((product.getName() == null) || product.getName().trim().isEmpty()) throw (new ValidateServiceException("El nombre es requerido"));
+        if(product.getName().length() > 100) throw (new ValidateServiceException(("El nombre debe tener máximo 100 caracteres")));
+        if(product.getPrice() == null) throw (new ValidateServiceException(("El precio es requerido")));
+        if(product.getPrice() <= 0) throw (new ValidateServiceException(("El precio debe tener un valor mayor o igual a cero (0)")));
         }
     }
