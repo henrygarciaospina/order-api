97f0cd7d8e2cd74f4334c599dd8e17ee49b4f1af
henrygosling <henrygarciaospina@hotmail.com>
2022-07-21 16:41:41 -0500

    Implementación de OrderConverter entitity <--> dto

--- src/main/java/com/codmind/orderapi/converters/ProductConverter.java
+++ src/main/java/com/codmind/orderapi/converters/ProductConverter.java
@@ -6,6 +6,8 @@ import com.codmind.orderapi.entity.Product;
 public class ProductConverter extends AbstractConverter<Product, ProductDTO>{
     @Override
     public ProductDTO fromEntity(Product entity) {
+        if(entity == null) return null;
+
         return ProductDTO.builder()
                 .id(entity.getId())
                 .name(entity.getName())
@@ -14,6 +16,8 @@ public class ProductConverter extends AbstractConverter<Product, ProductDTO>{
     }
     @Override
     public Product fromDTO(ProductDTO dto) {
+        if(dto == null) return null;
+
         return Product.builder()
                 .id(dto.getId())
                 .name(dto.getName())
