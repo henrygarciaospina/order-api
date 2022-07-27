ac613031e099c6fda30c04fd8d5126a67f16875d
henrygosling <henrygarciaospina@hotmail.com>
2022-07-27 12:09:36 -0500

    Creación entidad User y su repositorio

---
+++ src/main/java/com/codmind/orderapi/entity/User.java
@@ -0,0 +1,39 @@
+package com.codmind.orderapi.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="USERS")
+public class User {
+    @Id
+    @Column(name="ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USERNAME", length = 30, nullable = false)
+    private String username;
+
+    @Column(name = "PASSWORD", length = 150, nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
